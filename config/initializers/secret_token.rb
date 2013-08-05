b8e27483e8fcd9f802f9f8d605f77b2bdd32dfb3
hzycaicai2012 <hzycaicai@163.com>
2013-08-05 19:21:01 +0800

    Initial commit,try rails~

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lovepp::Application.config.secret_key_base = '5d4d1af6b005d498264bb249db6a451624a12363f59b277a9ea5a8f901d49eb273ae6880df13a5f1a3ea817f6b73f2c1f221d9e1857e938676c3122ead0732fe'
